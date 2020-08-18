25149dc170e4b916fa5cb8f573b1a97422c439ca
Aizaz <aizazathar678@gmail.com>
2020-08-19 02:39:21 +0500

    remove all files

--- family-task-test-master/Sql/dbo/tables/Task.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[Task] (
-    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
-    [Subject] VARCHAR(255) NOT NULL,
-    [IsComplete] bit NOT NULL,
-    [AssignedToId] UNIQUEIDENTIFIER,
-    CONSTRAINT fk_Task_Member_Id
-                        FOREIGN KEY ([AssignedToId])
-                          REFERENCES [dbo].[Member] ([Id])
-)
--- family-task-test-master/WebClient/Shared/Models/TaskModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class TaskModel
-{
-    public Guid id {get; set;}
-    public FamilyMember member { get; set; }
-    public string text { get; set; }
-    public bool isDone { get; set; }
-}
